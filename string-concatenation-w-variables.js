f27c503e58849c5739cabd24dad2511d0279c043
Travis Paul Pyle <travispyle@gmail.com>
2025-11-05 13:43:01 -0500

    Add files via upload
    string-concatenation-w-variables

---
+++ string-concatenation-w-variables.js
@@ -0,0 +1,10 @@
+// String Concatenation with Variables
+
+// Create a variable named favoriteAnimal and set it equal to your favorite animal.
+
+let favoriteAnimal = 'Red Nosed Pitbull'
+
+// Use console.log() to print 'My favorite animal: ANIMAL' to the console.
+// Use string concatenation so that ANIMAL is replaced with the value in the favoriteAnimal variable.
+
+console.log('My favorite animal: ' + favoriteAnimal)
