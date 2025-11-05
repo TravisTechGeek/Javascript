977c9505bf2fc65b18dc99405331e1aea6d2fb49
Travis Paul Pyle <travispyle@gmail.com>
2025-11-05 13:30:25 -0500

    Add files via upload
    Variables practice with increment operators

---
+++ variables.js
@@ -0,0 +1,10 @@
+// -- is used for a decrease by 1
+//++ is used for an increase by 1
+let gainedDollar = 3;
+let lostDollar = 50;
+// Using the increment operator, increase the value of gainedDollar.
+gainedDollar++;
+console.log(gainedDollar)
+// Using the decrement operator, decrease the value of lostDollar.
+lostDollar--;
+console.log(lostDollar)
