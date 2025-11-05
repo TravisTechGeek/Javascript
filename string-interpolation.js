3bbef487f27fec56ee795feeb86dc3a825c2be86
Travis Paul Pyle <travispyle@gmail.com>
2025-11-05 13:55:23 -0500

    Add files via upload
    String Interpolation

---
+++ string-interpolation.js
@@ -0,0 +1,12 @@
+// String Interpolation
+
+// use tickbacks which is above the tab key on macs
+// Create a variable called myName and assign it your name.
+let myName = 'Travis Paul Pyle';
+// Create a variable called myCity and assign it your favorite city’s name.
+let myCity = 'South Miami';
+// Use a single template literal to interpolate your variables into the sentence below. Use console.log() to print your sentence to the console in the following format:
+// My name is NAME. My favorite city is CITY.
+// Replace NAME and CITY in the string above by interpolating the values saved to myName and myCity.
+console.log(`My name is ${myName}. My favorite city is ${myCity}.`)
+
