9202664bfcaba487a26b7106489a46bee217d36b
Travis Paul Pyle <travispyle@gmail.com>
2025-11-05 16:29:16 -0500

    Add files via upload
    typeof variable operators

---
+++ typeof variable operators.js
@@ -0,0 +1,9 @@
+let newVariable = 'Playing around with typeof.';
+// Use console.log() to print the typeof newVariable.
+console.log(typeof newVariable)
+// Great, now let’s check what happens if we reassign the variable.
+// Below the console.log() statement, reassign newVariable to 1.
+newVariable = 1
+// Since you assigned this new value to newVariable, it has a new type! 
+// On the line below the reassignment, use console.log() to print typeof newVariable again.
+console.log(typeof newVariable)
