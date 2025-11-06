19ce5399696199fff09ed8b0d0ea4cb40dfe0c3d
Travis Paul Pyle <travispyle@gmail.com>
2025-11-06 14:18:44 -0500

    Add files via upload
    functions
    parameters and arguments

---
+++ Functions-Parameters-and-Aruguments.js
@@ -0,0 +1,13 @@
+// Javascript
+// Functions
+//Parameters and Arguments
+
+
+// The sayThanks() function works well, but let’s add the customer’s name in the message.
+// Add a parameter called name to the function declaration for sayThanks().
+
+function sayThanks(name) {
+  console.log('Thank you for your purchase, ' + name + '! We appreciate your business.');
+}
+// A customer named Cole just purchased something from the online store. Call sayThanks() and pass 'Cole' as an argument to send Cole a personalized thank-you message.
+sayThanks('Cole')
