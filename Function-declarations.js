53a1c4770daab4236f2101fb917aa7b3cec8d12a
Travis Paul Pyle <travispyle@gmail.com>
2025-11-06 13:53:10 -0500

    Add files via upload

---
+++ Function-declarations.js
@@ -0,0 +1,20 @@
+// Javascript
+// Functions
+// Function Declaration
+
+// Let’s create a function that prints a reminder to the console. Using a function declaration, create a function called getReminder().
+// In the function body of getReminder(), log the following reminder to the console: 'Water the plants.'
+// Let’s create another function that prints a useful Spanish travel phrase to the console.
+// Using a function declaration, create a function called greetInSpanish().
+// add code to the function body of greetInSpanish():
+// In the function body, console.log() the following Spanish phrase to the console: 'Buenas tardes.'.
+getReminder();
+
+function getReminder(){
+  console.log('Water the plants.')
+}
+greetInSpanish();
+
+function greetInSpanish(){
+  console.log('Buenas tardes.')
+}
