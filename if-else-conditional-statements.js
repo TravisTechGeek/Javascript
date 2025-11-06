a52e54c842022b2b2e4b22c274bc42208ae65a13
Travis Paul Pyle <travispyle@gmail.com>
2025-11-05 20:06:10 -0500

    Add files via upload
    Conditional Statements
    If/Else Statements

---
+++ if-else-conditional-statements.js
@@ -0,0 +1,15 @@
+// Javascript
+// if-else statements
+// conditional statements
+
+
+let sale = true;
+
+sale = false;
+
+if(sale) {
+  console.log('Time to buy!');
+}else {
+  console.log('Time to wait for a sale.')
+}
+//Add an else statement to the existing if statement. Inside the code block of the else statement, console.log() the string 'Time to wait for a sale.'
