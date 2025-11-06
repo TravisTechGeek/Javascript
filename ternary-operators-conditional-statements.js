c263611af85860770391100c8afbf2d46d8010a0
Travis Paul Pyle <travispyle@gmail.com>
2025-11-06 09:59:01 -0500

    Add files via upload
    Ternary Operators
    Conditional statements

---
+++ ternary-operators-conditional-statements.js
@@ -0,0 +1,19 @@
+// Javascript
+// conditional statements
+// ternary operator
+
+
+
+let isLocked = false;
+// Refactor, or edit, the first if...else block to use a ternary operator.
+isLocked ? console.log('You will need a key to open the door.') : console.log('You will not need a key to open the door.');
+
+
+let isCorrect = true;
+// Refactor the second if...else block to use a ternary operator.
+isCorrect ? console.log('Correct!') : console.log('Incorrect!');
+
+
+let favoritePhrase = 'Love That!';
+// Refactor the third if...else block to use a ternary operator.
+favoritePhrase === 'Love That!' ? console.log('I love that!') : console.log("I don't love that!");
