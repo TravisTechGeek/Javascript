f7f7b8c290638ce38d2092e6379125a6c403aadc
Travis Paul Pyle <travispyle@gmail.com>
2025-11-05 19:59:04 -0500

    Add files via upload
    Conditional Statements, specifically IF statements

---
+++ If-statements-conditional-statements.js
@@ -0,0 +1,15 @@
+// Conditional Statements
+// IF Statements
+// Using the let keyword, declare a variable named sale. Assign the value true to it.
+let sale = true
+// Now create an if statement. Provide the if statement with a condition of sale.
+// Inside the code block of the if statement, console.log() the string 'Time to buy!'.
+sale = false
+if (sale) {
+  console.log('Time to buy!')
+}
+
+// Notice that the code inside the if statement ran, since 'Time to buy!' was logged to the console.
+
+// Below the sale variable declaration, but before the if statement, reassign sale to false. 
+// Run your code and observe what happens, we’ll be changing this behavior in the next exercise.
