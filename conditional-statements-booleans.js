bcc3401f6556489c9624f6bf9d25ee498223d0a2
Travis Paul Pyle <travispyle@gmail.com>
2025-11-06 09:39:50 -0500

    Add files via upload
    Conditional Statements
    Booleans

---
+++ conditional-statements-booleans.js
@@ -0,0 +1,30 @@
+// Javascript
+// Conditional Statements
+// True and False booleans
+
+// Change the value of wordCount so that it is truthy. This value should still be a number.
+
+// After you make this change and run your code, 'Great! You've started your work!' should log to the console.
+
+// Change the value of favoritePhrase so that it is still a string but falsy.
+
+// After you make this change and run your code, 'This string is definitely empty.' should log to the console.
+
+
+
+let wordCount = 34;
+
+if (wordCount) {
+  console.log("Great! You've started your work!");
+} else {
+  console.log('Better get to work!');
+}
+
+
+let favoritePhrase = '';
+
+if (favoritePhrase) {
+  console.log("This string doesn't seem to be empty.");
+} else {
+  console.log('This string is definitely empty.');
+}
