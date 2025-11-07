c61573dd1dca9dff194d40afe612278bfad00cad
Travis Paul Pyle <travispyle@gmail.com>
2025-11-07 17:48:46 -0500

    Add files via upload
    Functions
    Concise Body Arrow Functions

---
+++ functions-concise-body-arrow-functions.js
@@ -0,0 +1,8 @@
+// Javascript
+// Functions
+// Consise Body Arrow Functions
+
+// Let’s refactor plantNeedsWater() to be a concise body. Notice that we’ve already converted the if/else statement to a ternary operator to make the code fit on one line.
+
+]const plantNeedsWater = day => 
+  day === 'Wednesday' ? true : false;
