05d9ad402eb772cb489528003cd1d1ebdf88f12d
Travis Paul Pyle <travispyle@gmail.com>
2025-11-10 14:10:37 -0500

    Add files via upload

---
+++ loops-looping-in-reverse.js
@@ -0,0 +1,10 @@
+// Javascript
+// Loops
+// Looping In Reverse
+
+// The loop below loops from 0 to 3. Edit it to loop backwards from 3 to 0
+
+/*Update the for loop so that it loops backwards, printing 3 to 0 to the console, including both 3 and 0. Make sure to adjust all three expressions to achieve this, and be careful to avoid an infinite loop!*/
+for (let counter = 3; counter >= 0; counter--) {
+  console.log(counter);
+}
