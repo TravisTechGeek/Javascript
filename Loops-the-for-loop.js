b7f8929da8aafae0a91a0ef75522510e82fb0039
Travis Paul Pyle <travispyle@gmail.com>
2025-11-10 14:06:36 -0500

    Add files via upload

---
+++ Loops-the-for-loop.js
@@ -0,0 +1,12 @@
+// Javascript
+// Loops
+// The For Loop
+
+/* Now, let’s make our own loop! Create for loop that iterates from 5 to 10 and logs each number to the console, including both 5 and 10.
+When writing/changing loops, there is a chance we will get a dreaded infinite loop, which essentially stops our programming from running anything else! This occurs when the loop’s stopping condition is never met, causing the loop to continue iterating, blocking the rest of the program from executing.
+To exit an infinite loop in an exercise, refresh the page — then fix the code for your loop before running it again.*/
+
+
+for (let counter = 5; counter < 11; counter++) {
+  console.log(counter);
+}
