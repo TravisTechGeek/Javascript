542bf70c728af91fa5cb2ce67bf7ef55a6c61ffd
Travis Paul Pyle <travispyle@gmail.com>
2025-11-10 12:24:16 -0500

    Add files via upload

---
+++ the-pop-method.js
@@ -0,0 +1,10 @@
+// Javascript
+// arrays
+// the .pop() method
+
+const chores = ['wash dishes', 'do laundry', 'take out trash', 'cook dinner', 'mop floor'];
+
+// Use the .pop() method to remove the last element from chores
+chores.pop('mop floor')
+// In a line after you called chores.pop(), log chores to the console to make sure it worked.
+console.log(chores)
