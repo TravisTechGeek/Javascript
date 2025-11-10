c94e4bce3679cee5016596cb41c3b84fe0bb888a
Travis Paul Pyle <travispyle@gmail.com>
2025-11-10 12:12:14 -0500

    Add files via upload
    the .length property

---
+++ the-length-properties.js
@@ -0,0 +1,8 @@
+// Javascript
+// Arrays 
+// the .length property
+
+// Find the length of the objectives array and log it to the console.
+const objectives = ['Learn a new language', 'Read 52 books', 'Run a marathon'];
+
+console.log(objectives.length)
