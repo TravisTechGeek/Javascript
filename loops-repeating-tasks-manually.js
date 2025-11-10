4706cb353c425f0165bfc57578a7bdaabc154ea6
Travis Paul Pyle <travispyle@gmail.com>
2025-11-10 14:00:15 -0500

    Add files via upload
    Loops
    repeating tasks manually

---
+++ loops-repeating-tasks-manually.js
@@ -0,0 +1,13 @@
+// Javascript
+// Loops
+// Repeating Tasks Manually
+
+// Create the variable vacationSpots, and assign its value to an array of three strings naming places you’d like to visit.
+vacationSpots = ['London', 'Germany', 'Brasil']
+
+// Next, console.log() each item in vacationSpots. Since we don’t know loops yet, we have to console.log() each element in the array separately.
+console.log(vacationSpots[0]);
+console.log(vacationSpots[1]);
+console.log(vacationSpots[2]);
+
+// Nice work! Now imagine that the vacation list had 100 places on it — logging each array element to the console by hand would be a tedious task! In the next exercise, we will learn how to make things more efficient with for loops.
