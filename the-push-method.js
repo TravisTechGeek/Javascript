9e8b3fb94916a35d3025b12615e0dcd712465c4e
Travis Paul Pyle <travispyle@gmail.com>
2025-11-10 12:16:48 -0500

    Add files via upload
    the .push() method

---
+++ the-push-method.js
@@ -0,0 +1,11 @@
+// Javascript
+// Arrays
+// the .push() method
+
+const chores = ['wash dishes', 'do laundry', 'take out trash'];
+
+// Add two elements to the chores array using .push().
+chores.push('sweep the chimney', 'scrub the floor')
+
+// Use console.log() to print your chores array to make sure your items were added.
+console.log(chores)
