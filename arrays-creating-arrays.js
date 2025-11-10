b9482101d08b92d4b940f828b4f96e8bcafd7ce2
Travis Paul Pyle <travispyle@gmail.com>
2025-11-10 10:33:50 -0500

    Add files via upload

---
+++ arrays-creating-arrays.js
@@ -0,0 +1,8 @@
+// Javascript
+// Arrays
+// Create an array
+
+// Declare a variable using const named hobbies and set it equal to an array with three strings inside of it.
+const hobbies = ['swimming', 'gaming', 'coding']
+// Use console.log() to print hobbies to the console.
+console.log(hobbies)
